11e06fb279fb13d2a8ace82d541ad7514e5f5d26
realerikrani <realerikrani@tuta.io>
2019-05-19 13:10:58 +0300

    Extract price and info to before block

--- tests/unit/ProductItem.spec.js
+++ tests/unit/ProductItem.spec.js
@@ -2,32 +2,32 @@ import { expect } from 'chai';
 import { shallowMount } from '@vue/test-utils';
 import ProductItem from '@/components/ProductItem.vue';
 
+
 describe('ProductItem.vue', () => {
+  before(() => {
+    this.price = 'any price';
+    this.info = 'any info';
+  });
+
   it('renders price and info', () => {
     // given
-    const price = 'any price';
-    const info = 'any info';
+    const propsData = { price: this.price, info: this.info };
 
     // when
-    const wrapper = shallowMount(ProductItem, { propsData: { price, info } });
+    const wrapper = shallowMount(ProductItem, { propsData });
 
     // then
-    expect(wrapper.find('.product__price').text()).to.include(price);
-    expect(wrapper.find('.product__info').text()).to.include(info);
+    expect(wrapper.find('.product__price').text()).to.include(this.price);
+    expect(wrapper.find('.product__info').text()).to.include(this.info);
   });
 
   it('renders extra info when it is given', () => {
     // given
-    const price = 'any price';
-    const info = 'any info';
     const extraInfo = 'any extra info';
+    const propsData = { price: this.price, info: this.info, extraInfo };
 
     // when
-    const wrapper = shallowMount(ProductItem, {
-      propsData: {
-        price, info, extraInfo,
-      },
-    });
+    const wrapper = shallowMount(ProductItem, { propsData });
 
     // then
     expect(wrapper.find('.product__extra').text()).to.include(extraInfo);
@@ -35,11 +35,10 @@ describe('ProductItem.vue', () => {
 
   it('renders no extra info when it is missing', () => {
     // given
-    const price = 'any price';
-    const info = 'any info';
+    const propsData = { price: this.price, info: this.info };
 
     // when
-    const wrapper = shallowMount(ProductItem, { propsData: { price, info } });
+    const wrapper = shallowMount(ProductItem, { propsData });
 
     // then
     expect(wrapper.find('.product__extra').exists()).to.be.equal(false);
