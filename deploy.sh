d88ec22fdec376a4e66d8560db379ff8c1bd30e4
realerikrani <realerikrani@tuta.io>
2019-05-19 10:17:55 +0300

    Add deployment to gh-pages

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+set -e
+npm run build
+cd dist
+
+git init
+
+git config --global user.email "33372685+erikrani@users.noreply.github.com"
+git config --global user.name "erikrani"
+
+git add -A
+git commit -m 'deploy'
+
+git push --quiet --force "erikrani:${GH_TOKEN}@github.com/realerikrani/price-list.git" master:gh-pages
+
+cd - || exit
