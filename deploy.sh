b7a9d25972e1d296e050c6defed05b44d2be03a4
realerikrani <realerikrani@tuta.io>
2019-05-19 10:24:56 +0300

    Add origin in deploy script

--- deploy.sh
+++ deploy.sh
@@ -9,9 +9,11 @@ git init
 git config --global user.email "33372685+erikrani@users.noreply.github.com"
 git config --global user.name "erikrani"
 
+git remote add origin "https://erikrani:${GH_TOKEN}@github.com/realerikrani/price-list.git"
+
 git add -A
 git commit -m 'deploy'
 
-git push --quiet --force "erikrani:${GH_TOKEN}@github.com/realerikrani/price-list.git" master:gh-pages
+git push --quiet --force origin master:gh-pages
 
 cd - || exit
