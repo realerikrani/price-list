c39a943b974a17c485f24058169f27a1ca83173e
Erik Räni <erik.rani@messente.com>
2021-01-30 11:42:51 +0200

    Add a notice to the top of the page (#101)
    Defaults to "Effective <today's date>"

--- main.go
+++ main.go
@@ -5,13 +5,21 @@ import (
 	"flag"
 	"log"
 	"text/template"
+	"time"
 
 	"github.com/realerikrani/price-list/creator"
 )
 
 func main() {
-	inputPtr := flag.String("input", "./price-list.json", "a path to a JSON file")
-	outputPtr := flag.String("output", "./price-list.html", "an output path for HTML")
+	input := flag.String("input", "./price-list.json", "a path to a JSON file")
+	output := flag.String("output", "./price-list.html", "an output path for HTML")
+	dt := time.Now()
+
+	notice := flag.String(
+		"notice",
+		"Effective "+dt.Format("2 January 2006"),
+		"a notice on top of the page",
+	)
 	flag.Parse()
 
 	//go:embed tmplhtml/list.gohtml
@@ -32,7 +40,7 @@ func main() {
 	tmpl.Parse(groupHeaderHTML)
 	tmpl.Parse(groupsHTML)
 
-	if err := creator.CreatePriceList(*inputPtr, *outputPtr, *tmpl); err != nil {
+	if err := creator.CreatePriceList(*input, *output, *notice, *tmpl); err != nil {
 		log.Fatal(err)
 	}
 }
